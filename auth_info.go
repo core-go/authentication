ead5dbb60883a99da062756f1771b012f8684f99
thanh phu <htp1912@gmail.com>
2021-02-01 13:12:32 +0700

    update config auth info

--- auth_info.go
+++ auth_info.go
@@ -2,8 +2,10 @@ package auth
 
 type AuthInfo struct {
 	Step       int    `json:"step,omitempty" gorm:"column:step" bson:"step,omitempty" dynamodbav:"step,omitempty" firestore:"step,omitempty"`
-	Username   string `json:"username,omitempty" gorm:"column:username" bson:"username,omitempty" dynamodbav:"username,omitempty" firestore:"username,omitempty"`
+	Username   string `json:"username,omitempty" gorm:"column:username;primary_key" bson:"username,omitempty" dynamodbav:"username,omitempty" firestore:"username,omitempty"`
 	Password   string `json:"password,omitempty" gorm:"column:password" bson:"password,omitempty" dynamodbav:"password,omitempty" firestore:"password,omitempty"`
 	Passcode   string `json:"passcode,omitempty" gorm:"column:passcode" bson:"passcode,omitempty" dynamodbav:"passcode,omitempty" firestore:"passcode,omitempty"`
+	Ip         string `json:"ip,omitempty" gorm:"column:ip" bson:"ip,omitempty" dynamodbav:"ip,omitempty" firestore:"ip,omitempty"`
+	Device     string `json:"device,omitempty" gorm:"column:device" bson:"device,omitempty" dynamodbav:"device,omitempty" firestore:"device,omitempty"`
 	SenderType string `json:"senderType,omitempty" gorm:"column:sendertype" bson:"senderType,omitempty" dynamodbav:"senderType,omitempty" firestore:"senderType,omitempty"`
 }
