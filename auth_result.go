a7aab8cb37818cb487b250f843ab07cd56560e44
Duc Nguyen <minhduc140583@gmail.com>
2021-02-21 11:58:23 +0700

    Refactor result status

--- auth_result.go
+++ auth_result.go
@@ -1,7 +1,7 @@
 package auth
 
 type AuthResult struct {
-	Status  AuthStatus   `json:"status" gorm:"column:status" bson:"status" dynamodbav:"status" firestore:"status"`
+	Status  int          `json:"status" gorm:"column:status" bson:"status" dynamodbav:"status" firestore:"status"`
 	User    *UserAccount `json:"user,omitempty" gorm:"column:user" bson:"user,omitempty" dynamodbav:"user,omitempty" firestore:"user,omitempty"`
 	Message string       `json:"message,omitempty" gorm:"column:message" bson:"message,omitempty" dynamodbav:"message,omitempty" firestore:"message,omitempty"`
 }
--- auth_status.go
+++ auth_status.go
@@ -1,7 +1,96 @@
 package auth
 
-type AuthStatus int
+type AuthStatusConfig struct {
+	Fail                  *int `mapstructure:"fail"`
+	Success               *int `mapstructure:"success"`
+	SuccessAndReactivated *int `mapstructure:"success_and_reactivated"`
+	TwoFactorRequired     *int `mapstructure:"two_factor_required"`
+	WrongPassword         *int `mapstructure:"wrong_password"`
+	PasswordExpired       *int `mapstructure:"password_expired"`
+	AccessTimeLocked      *int `mapstructure:"access_time_locked"`
+	Locked                *int `mapstructure:"locked"`
+	Suspended             *int `mapstructure:"suspended"`
+	Disabled              *int `mapstructure:"disabled"`
+	Error                 *int `mapstructure:"Error"`
+}
+type AuthStatus struct {
+	Fail                  int `mapstructure:"fail"`
+	Success               int `mapstructure:"success"`
+	SuccessAndReactivated int `mapstructure:"success_and_reactivated"`
+	TwoFactorRequired     int `mapstructure:"two_factor_required"`
+	WrongPassword         int `mapstructure:"wrong_password"`
+	PasswordExpired       int `mapstructure:"password_expired"`
+	AccessTimeLocked      int `mapstructure:"access_time_locked"`
+	Locked                int `mapstructure:"locked"`
+	Suspended             int `mapstructure:"suspended"`
+	Disabled              int `mapstructure:"disabled"`
+	Error                 int `mapstructure:"error"`
+}
 
+func InitStatus(c AuthStatusConfig) AuthStatus {
+	var s AuthStatus
+	if c.Error != nil {
+		s.Error = *c.Error
+	} else {
+		s.Error = 4
+	}
+	if c.Fail != nil {
+		s.Fail = *c.Fail
+	} else {
+		s.Fail = 0
+	}
+	if c.Success != nil {
+		s.Success = *c.Success
+	} else {
+		s.Success = 1
+	}
+	if c.SuccessAndReactivated != nil {
+		s.SuccessAndReactivated = *c.SuccessAndReactivated
+	} else {
+		s.SuccessAndReactivated = s.Success
+	}
+	if c.TwoFactorRequired != nil {
+		s.TwoFactorRequired = *c.TwoFactorRequired
+	} else {
+		s.TwoFactorRequired = 2
+	}
+	if c.WrongPassword != nil {
+		s.WrongPassword = *c.WrongPassword
+	} else {
+		s.WrongPassword = s.Fail
+	}
+	if c.PasswordExpired != nil {
+		s.PasswordExpired = *c.PasswordExpired
+	} else {
+		s.PasswordExpired = s.Fail
+	}
+	if c.AccessTimeLocked != nil {
+		s.AccessTimeLocked = *c.AccessTimeLocked
+	} else {
+		s.AccessTimeLocked = s.Fail
+	}
+	if c.Locked != nil {
+		s.Locked = *c.Locked
+	} else {
+		s.Locked = s.Fail
+	}
+	if c.Suspended != nil {
+		s.Suspended = *c.Suspended
+	} else {
+		s.Suspended = s.Fail
+	}
+	if c.Disabled != nil {
+		s.Disabled = *c.Disabled
+	} else {
+		s.Disabled = s.Fail
+	}
+	if c.Error == nil && s.Error != 4 {
+		s.Error = s.Fail
+	}
+	return s
+}
+
+/*
 const (
 	StatusSuccess               = AuthStatus(0)
 	StatusSuccessAndReactivated = AuthStatus(1)
@@ -15,3 +104,4 @@ const (
 	StatusDisabled              = AuthStatus(9)
 	StatusSystemError           = AuthStatus(10)
 )
+*/
