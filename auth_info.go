df29cf1dcd277de93253431a201aecbbb0d21af3
Duc Nguyen <minhduc140583@gmail.com>
2021-04-25 18:51:22 +0700

    Fix bug json

--- auth_info.go
+++ auth_info.go
@@ -2,7 +2,7 @@ package auth
 
 type AuthInfo struct {
 	Step     int    `mapstructure:"step" json:"step,omitempty" gorm:"column:step" bson:"step,omitempty" dynamodbav:"step,omitempty" firestore:"step,omitempty"`
-	Username string `mapstructure:"username" son:"username,omitempty" gorm:"column:username;primary_key" bson:"username,omitempty" dynamodbav:"username,omitempty" firestore:"username,omitempty"`
+	Username string `mapstructure:"username" json:"username,omitempty" gorm:"column:username;primary_key" bson:"username,omitempty" dynamodbav:"username,omitempty" firestore:"username,omitempty"`
 	Password string `mapstructure:"password" json:"password,omitempty" gorm:"column:password" bson:"password,omitempty" dynamodbav:"password,omitempty" firestore:"password,omitempty"`
 	Passcode string `mapstructure:"passcode" json:"passcode,omitempty" gorm:"column:passcode" bson:"passcode,omitempty" dynamodbav:"passcode,omitempty" firestore:"passcode,omitempty"`
 	Ip       string `mapstructure:"ip" json:"ip,omitempty" gorm:"column:ip" bson:"ip,omitempty" dynamodbav:"ip,omitempty" firestore:"ip,omitempty"`
