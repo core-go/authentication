b90d6701885a8ddd6f6de644e3461bccba0827f5
Duc Nguyen <minhduc140583@gmail.com>
2021-10-10 23:21:42 +0700

    Refactor respond JSON'

--- respond.go
+++ respond.go
@@ -8,12 +8,12 @@ import (
 
 const internalServerError = "Internal Server Error"
 
-func respond(w http.ResponseWriter, r *http.Request, code int, result interface{}, writeLog func(context.Context, string, string, bool, string) error, resource string, action string, success bool, desc string) {
-	response, _ := json.Marshal(result)
+func respond(w http.ResponseWriter, r *http.Request, code int, result interface{}, writeLog func(context.Context, string, string, bool, string) error, resource string, action string, success bool, desc string) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	w.Write(response)
+	err := json.NewEncoder(w).Encode(result)
 	if writeLog != nil {
 		writeLog(r.Context(), resource, action, success, desc)
 	}
+	return err
 }
